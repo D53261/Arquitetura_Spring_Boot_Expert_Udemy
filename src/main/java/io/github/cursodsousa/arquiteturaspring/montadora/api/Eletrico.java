875b6a0a68581bd390ae89d596f07090d1cd894d
D53261 <danilo051007@gmail.com>
2025-08-15 12:30:16 -0300

    adicionando comentarios extras

--- src/main/java/io/github/cursodsousa/arquiteturaspring/montadora/api/Eletrico.java
+++ src/main/java/io/github/cursodsousa/arquiteturaspring/montadora/api/Eletrico.java
@@ -7,6 +7,12 @@ import java.lang.annotation.Retention; // Importa a política de retenção da a
 import java.lang.annotation.RetentionPolicy; // Importa as opções de política de retenção
 import java.lang.annotation.Target; // Importa o alvo da anotação
 
+// Um Qualifier é usado para diferenciar entre múltiplos beans do mesmo tipo
+// quando se está fazendo a injeção de dependência no Spring.
+// Neste caso, a anotação @Eletrico será usada para identificar o bean específico
+// que representa um motor elétrico, permitindo que o Spring saiba exatamente qual
+// bean injetar quando essa anotação for usada.
+
 @Retention(RetentionPolicy.RUNTIME) // Indica que a anotação estará disponível em tempo de execução
 @Target({ElementType.METHOD, ElementType.FIELD}) // Permite uso em métodos e campos
 @Qualifier("motorEletrico") // Marca como Qualifier para identificar beans específicos
